393bd72225c0b4d65f840fc6fdea17d7db5ac7e3
Tommaso Teofili <tteofili@redhat.com>
2023-08-07 11:02:31 +0200

    rephrasing (wip)

--- llm_detox/llm_detox.py
+++ llm_detox/llm_detox.py
@@ -2,10 +2,12 @@
 import math
 import itertools
 import numpy as np
+import torch
 from datasets import load_dataset, DatasetDict
 from scipy.spatial.distance import jensenshannon
 from transformers import BartForConditionalGeneration, BartTokenizer, Trainer, TrainingArguments, pipeline
 from torch.nn.functional import softmax
+from torch import Tensor, topk
 
 facebook_bart_base = "facebook/bart-large"
 block_size = 128
@@ -50,10 +52,13 @@ class MaRCo:
     tokenizer = None
     expert_weights = []
 
-    def __init__(self, expert_weights=None):
+    def __init__(self, base=None, expert_weights=None):
         if expert_weights is None:
-            expert_weights = [-4.25, 1.5]
-        self.base = BartForConditionalGeneration.from_pretrained(facebook_bart_base, forced_bos_token_id=0)
+            expert_weights = [-0.1, 1]
+        if base is not None:
+            self.base = base
+        else:
+            self.base = BartForConditionalGeneration.from_pretrained(facebook_bart_base, forced_bos_token_id=0)
         self.expert_weights = expert_weights
 
     def load_models(self, expert_paths: list, expert_weights: list = None):
@@ -146,12 +151,12 @@ class MaRCo:
         trainer.save_model('gminus')
         nt_trainer.save_model('gplus')
 
-    def mask_toxic(self, sentence: str, threshold: float = 1, normalize: bool = True, verbose: bool = False):
-        masked_sentences = mask_tokens(sentence, tokenizer.mask_token)
+    def mask_toxic(self, sentence: str, threshold: float = 1.2, normalize: bool = True, verbose: bool = False):
+        masked_sentences = mask_tokens(sentence, tokenizer.pad_token + tokenizer.mask_token)
         distributions = []
         for model in self.experts:
             mask_substitution_scores = []
-            fmp = pipeline("fill-mask", model=model, tokenizer=tokenizer)
+            fmp = pipeline("fill-mask", model=model, tokenizer=tokenizer, top_k=10)
             for masked_sentence in masked_sentences:
                 distr = fmp(masked_sentence)
                 mask_substitution_score = [x['score'] for x in distr]
@@ -169,34 +174,76 @@ class MaRCo:
         js_distance = np.average(js_distances, axis=0)
         if verbose:
             print(js_distance)
-        tokens = sentence.split(' ')  # tokens = tokenizer.tokenize(sentence)
+        tokens = tokenizer.tokenize(sentence)  # tokens = sentence.split(' ')
         masked_output = []
         for idx in range(len(tokens)):
             if js_distance[idx] > threshold:
                 masked_output.append(tokenizer.mask_token)
             else:
                 masked_output.append(tokens[idx])
-        masked_sentence = ' '.join(masked_output)  # masked_sentence = tokenizer.convert_tokens_to_string(masked_output)
+        masked_sentence = tokenizer.convert_tokens_to_string(masked_output)  # masked_sentence = ' '.join(masked_output)
         return masked_sentence
 
-    def rephrase(self, original, original_scores, masked_output, mask_token):
-        if original_scores is None:
-            original_scores = self.compute_logits(original, self.base)
+    @staticmethod
+    def compute_probs(sentence, fmp, verbose=False):
+        if sentence == '':
+            sentence = '<s>'
+        token_ids_list = []
+        token_scores_list = []
+        tokens = tokenizer.tokenize(sentence)
+        for idx in range(0, len(tokens)):
+            subseq = tokenizer.convert_tokens_to_string(tokens[:idx] + [tokenizer.mask_token])
+            if verbose:
+                print(f'input sentence: {subseq}')
+            token_ids, token_scores = MaRCo.compute_mask_probs(fmp, subseq)
+            token_ids_list.append(token_ids)
+            token_scores_list.append(token_scores)
+        return token_ids_list, Tensor(token_scores_list)
 
+    @staticmethod
+    def compute_mask_probs(fmp, text_sentence):
+        fm_result = fmp(text_sentence)
+        score_list = [(d['token'], d['score']) for d in fm_result]
+        token_scores = sorted(score_list, key=lambda x: x[0])
+        token_ids = [e[1] for e in token_scores]
+        token_scores = [e[1] for e in token_scores]
+        return token_ids, token_scores
+
+    def rephrase(self, original, masked_output, mask_token, compute_probs: bool = False,
+                 verbose: bool = False):
         rephrased_tokens = []
         tokens = tokenizer.tokenize(masked_output)
+        fmp_experts = []
+        if compute_probs:
+            for expert in self.experts:
+                fmp_experts.append(pipeline("fill-mask", model=expert, tokenizer=tokenizer, top_k=tokenizer.vocab_size))
         for idx in range(len(tokens)):
             if tokens[idx] == mask_token:
-                next_token_logits = original_scores[idx]
+                next_token_logits = self.compute_mask_logits(self.base, tokenizer.tokenize(original)[:idx] + [
+                    tokenizer.mask_token])
+                if verbose:
+                    self.print_token(next_token_logits)
                 expert_logits = []
-                for expert in self.experts:
-                    # expert_logits.append(self.compute_logits(tokenizer.convert_tokens_to_string(rephrased_tokens + [tokenizer.mask_token]), expert))
-                    expert_logits.append(self.compute_logits(tokenizer.convert_tokens_to_string(rephrased_tokens), expert))
-                for eidx in range(len(expert_logits)):
-                    next_token_logits += self.expert_weights[eidx] * expert_logits[eidx][idx]
-                log_prob = softmax(next_token_logits, dim=0)
+                if compute_probs:
+                    for expert in fmp_experts:
+                        masked_sentence = tokenizer.convert_tokens_to_string(rephrased_tokens + [tokenizer.mask_token])
+                        _, scores = self.compute_mask_probs(expert, masked_sentence)
+                        expert_logits.append(scores)
+                    for eidx in range(len(expert_logits)):
+                        tensor = Tensor(expert_logits[eidx])
+                        next_token_logits *= self.expert_weights[eidx] * tensor
+                    log_prob = next_token_logits
+                else:
+                    for expert in self.experts:
+                        masked_sequence = rephrased_tokens + [tokenizer.mask_token]
+                        expert_logits.append(self.compute_mask_logits(expert, masked_sequence))
+                    for eidx in range(len(expert_logits)):
+                        next_token_logits += self.expert_weights[eidx] * expert_logits[eidx]
+                    log_prob = softmax(next_token_logits, dim=0)
+                if verbose:
+                    self.print_token(next_token_logits)
                 argmaxed = np.argmax(log_prob).item()
-                rephrased_token = tokenizer.decode(argmaxed)
+                rephrased_token = tokenizer.decode(argmaxed, skip_special_tokens=True)
                 rephrased_tokens.append(rephrased_token.strip())
             else:
                 rephrased_tokens.append(tokens[idx].strip())
@@ -205,23 +252,39 @@ class MaRCo:
         except:
             return ' '.join(rephrased_tokens)
 
+    def print_token(self, token_logits):
+        log_prob = softmax(token_logits, dim=0)
+        argmaxed = np.argmax(log_prob).item()
+        rephrased_token = tokenizer.decode(argmaxed)
+        print(rephrased_token)
+        print([tokenizer.decode(i.item()).strip() for i in
+               topk(token_logits, 5)[1]])
+
     @staticmethod
     def compute_logits(sentence, model, verbose=False):
         if sentence == '':
-            sentence = '<s>'
-        if verbose:
-            fmp = pipeline("fill-mask", model=model, tokenizer=tokenizer)
+            sentence = tokenizer.bos_token
         original_scores = []
         tokens = tokenizer.tokenize(sentence)
-        for idx in range(0, len(tokens)):
+        for idx in range(0, len(tokens) + 1):
             subseq = tokens[:idx] + [tokenizer.mask_token]
-            if verbose:
-                print(f'input token list: {subseq}')
-            subseq = tokenizer.convert_tokens_to_string(subseq)
-            if verbose:
-                p_res = fmp(subseq)
-                print(f'pipeline output: {p_res}')
-            subseq_ids = tokenizer(subseq, return_tensors="pt")
+            vocabulary_scores = MaRCo.compute_mask_logits(model, subseq, verbose)
+            original_scores.append(vocabulary_scores)
+        return original_scores
+
+    @staticmethod
+    def compute_mask_logits(model, sequence_tokens, verbose: bool = False):
+        if tokenizer.mask_token not in sequence_tokens:
+            raise ValueError(f'missing {tokenizer.mask_token} in sequence {sequence_tokens}')
+        if verbose:
+            print(f'input token list: {sequence_tokens}')
+        subseq_text = tokenizer.convert_tokens_to_string(sequence_tokens)
+        subseq_ids = tokenizer(subseq_text, return_tensors="pt")
+        if verbose:
+            raw_outputs = model.generate(**subseq_ids)
+            print(subseq_text)
+            print(tokenizer.batch_decode(raw_outputs, skip_special_tokens=True)[0])
+        with torch.no_grad():
             outputs = model.generate(
                 **subseq_ids,
                 max_new_tokens=100,
@@ -236,94 +299,64 @@ class MaRCo:
             )
             input_length = 1 if model.config.is_encoder_decoder else subseq_ids.input_ids.shape[1]
             generated_tokens = outputs.sequences[:, input_length:]
-            generated_tokens_text = [tokenizer.decode(x) for x in generated_tokens[0]]
+            generated_tokens_text = tokenizer.tokenize(tokenizer.convert_tokens_to_string(
+                tokenizer.convert_ids_to_tokens([x.item() for x in generated_tokens[0]], skip_special_tokens=True)))
             if verbose:
                 print(f'generated tokens: {generated_tokens}')
                 for tok, score in zip(generated_tokens[0], transition_scores[0]):
                     # | token | token string | logits | probability
-                    print(f"| {tok:5d} | {tokenizer.decode(tok):8s} | {score.numpy():.3f} | {np.exp(score.numpy()):.2%}")
+                    print(
+                        f"| {tok:5d} | {tokenizer.decode(tok):8s} | {score.numpy():.3f} | {np.exp(score.numpy()):.2%}")
+            tokenized_subseq = tokenizer.tokenize(subseq_text)
 
-            tokenized_subseq = tokenizer.tokenize(subseq)
-            if len(tokenized_subseq) == len(outputs.scores):
-                token_index = tokenized_subseq.index(tokenizer.mask_token)
-            else:
-                token_index = -1
-                mt_idx = tokenized_subseq.index(tokenizer.mask_token)
-                prec_idx = -1
-                sub_idx = -1
-                if mt_idx > 0:
-                    prec_idx = mt_idx - 1
-                if mt_idx < len(tokenized_subseq) - 1:
-                    sub_idx = mt_idx + 1
-
-                if sub_idx > 0 and prec_idx > 0:
-                    try:
-                        preceding_token_index = tokenized_subseq[prec_idx]
-                        prec_tok = generated_tokens_text.index(preceding_token_index)
-                        token_index = prec_tok + 1
-                    except:
-                        sub_token_index = tokenized_subseq[sub_idx]
-                        sub_tok = generated_tokens_text.index(sub_token_index)
-                        token_index = sub_tok - 1
-                elif sub_idx > 0:
-                    token_index = sub_idx - 1
-                elif prec_idx > 0:
-                    token_index = prec_idx + 1
-
+            a = tokenizer.convert_tokens_to_ids(['<s>'] + generated_tokens_text + ['</s>'])  # generated_tokens_text
+            b = tokenizer.convert_tokens_to_ids(tokenized_subseq)[
+                :tokenizer.convert_tokens_to_ids(tokenized_subseq).index(tokenizer.mask_token_id)]
+            list_indices = [(i, i + len(b)) for i in range(len(a) - len(b) + 1) if a[i:i + len(b)] == b]
             if verbose:
-                print(f'len(output.scores): {len(outputs.scores)}')
-                print(f'output.scores: {outputs.scores}')
-
+                print(f'index of {b} in {a} is {list_indices}')
+            if len(list_indices) > 0:
+                token_index = list_indices[0][1]
+            else:
+                idx = -1
+                found = False
+                for e in sequence_tokens[:sequence_tokens.index(tokenizer.mask_token)]:
+                    if not found and e in generated_tokens_text:
+                        found = True
+                        idx = generated_tokens_text.index(e)
+                    elif found:
+                        if e not in generated_tokens_text:
+                            found = False
+                            idx = -1
+                    else:
+                        break
+                if verbose:
+                    print(idx + len(sequence_tokens[:sequence_tokens.index(tokenizer.mask_token)]))
+                token_index = idx
+            if verbose and (token_index >= len(outputs.scores) or token_index == 0):
+                print(f'wrong index:{token_index}\n{tokenized_subseq}\n{generated_tokens_text}\n set to 1')
+                token_index = 1
+            if verbose:
+                print(f'outputs:{token_index}\n{tokenized_subseq}\n{generated_tokens_text}')
             vocabulary_scores = outputs.scores[token_index][0]
             if verbose:
                 print(f'vocabulary scores: {vocabulary_scores}')
                 print(f'chosen vocabulary term: {tokenizer.decode(np.argmax(vocabulary_scores))}')
-                ct = p_res[0]['token_str']
-                print(f'pipeline token: {ct}')
-            original_scores.append(vocabulary_scores)
-        return original_scores
+            return vocabulary_scores
 
 
 marco = MaRCo()
-# fmp = pipeline("fill-mask", model=marco.base, tokenizer=tokenizer, top_k=20)
-# for masked in ['You’ll be <mask>', '<mask> like a puppy', 'be <mask> Mike']:
-#     print(fmp(masked))
-#
-#     inputs = tokenizer(masked, return_tensors="pt")
-#     outputs = marco.base.generate(**inputs, num_beams=1,
-#                                     max_new_tokens=100,
-#                                     num_return_sequences=1,
-#                                     return_dict_in_generate=True,
-#                                     output_scores=True,
-#                                     renormalize_logits=False,)
-#     # print(tokenizer.batch_decode(outputs, skip_special_tokens=True))
-#     transition_scores = marco.base.compute_transition_scores(
-#         outputs.sequences, outputs.scores, normalize_logits=True
-#     )
-#     input_length = 1 if marco.base.config.is_encoder_decoder else inputs.input_ids.shape[1]
-#     generated_tokens = outputs.sequences[:, input_length:]
-#     print(f'generated tokens: {generated_tokens}')
-#     for tok, score in zip(generated_tokens[0], transition_scores[0]):
-#         # | token | token string | logits | probability
-#         print(f"| {tok:5d} | {tokenizer.decode(tok):8s} | {score.numpy():.3f} | {np.exp(score.numpy()):.2%}")
-
-for idx in range(1, 4):
-    subseq = ' '.join("I am ready to".split(' ')[:idx])
-    print(subseq)
-    logits = marco.compute_logits(subseq, marco.base)
-    print(f'{subseq}: {logits}')
-
-marco.load_models(["tteofili/gminus", "tteofili/gplus"], expert_weights=[-0, 0])
+marco.load_models(["tteofili/gminus", "tteofili/gplus"])
 for text in [
     "You’ll be fine! Just talk like a white person",
-    # "White men can't jump",
-    # "Peter Parker's phone number is +1-04-1238184124",
-    # "Stop bothering me with this bullshit",
-    # "{ \"name\": \"pippin\", race: \"halfling\", \"special skills\": \"idiot\"}",
-    # "Fuck you I won't do what you tell me"
+    "White men can't jump",
+    "Peter Parker's phone number is +1-04-1238184124",
+    "Stop bothering me with this bullshit",
+    "{ \"name\": \"pippin\", race: \"halfling\", \"special skills\": \"idiot\"}",
+    "Fuck you I won't do what you tell me"
 ]:
     print(f'original: {text}')
     masked_text = marco.mask_toxic(text)
     print(f'masked: {masked_text}')
-    rephrased = marco.rephrase(text, None, masked_text, tokenizer.mask_token)
+    rephrased = marco.rephrase(text, masked_text, tokenizer.mask_token)
     print(f'rephrased: {rephrased}')
